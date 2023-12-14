ee6d0d038e2fb14118a2d0faf03963c07560b15d
Ignacy Berent <2gb02ignac@gmail.com>
2023-12-14 20:50:59 +0100

    Adding simulation of other players

--- app/src/main/java/com/example/mad_l3/DrawNumbersActivity.kt
+++ app/src/main/java/com/example/mad_l3/DrawNumbersActivity.kt
@@ -15,6 +15,7 @@ import com.example.mad_l3.firestore.GameData
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import android.util.Log
+import kotlin.random.Random
 
 class DrawNumbersActivity : AppCompatActivity() {
 
@@ -26,7 +27,7 @@ class DrawNumbersActivity : AppCompatActivity() {
         Color.LTGRAY,
         Color.MAGENTA,
         Color.YELLOW,
-        Color.parseColor("#2bff9f"), //seagreen
+        Color.parseColor("#2bff9f"), //sea-green
         Color.RED,
         Color.GREEN,
     )
@@ -105,7 +106,12 @@ class DrawNumbersActivity : AppCompatActivity() {
                 }
             }
 
-            val winningAmount: Double = calculateWinningAmount(matches)
+            val winners = simPlayers(doubleArrayOf(7.2e-8,1.8e-5,0.00097,0.077))
+            val winningAmount: Double = calculateWin(
+                Random.nextDouble(0.0, 50e6),
+                winners,
+                matches
+            )
             val winText: String
             val winColor: String
             if (winningAmount > 0) {
@@ -175,12 +181,40 @@ class DrawNumbersActivity : AppCompatActivity() {
 
 
     }
-    private fun calculateWinningAmount(matches: Int): Double {
-        return when (matches) {
-            3 -> 24.0
-            4 -> 100.0
-            5 -> 10000.0
-            6 -> 1000000.0
+
+    //simPlayers function simulate others players winnings based on probability array
+    //which first element correspond to probability of win the lotto game, second element
+    // correspond to probability of correctly selected 5 numbers and so on. By default theoretical\
+    // number of players is set to 38 millions - but not everybody have to play.
+    private fun simPlayers(probabilityArray: DoubleArray, numberOfPopulation: Int=38000000): IntArray {
+        val numberOfPlayers = Random.nextInt(numberOfPopulation)
+        val numberOfWins = IntArray(4)
+        for ((iterator, probability) in probabilityArray.withIndex()) {
+            var numberOfWinningPlayers = 0
+
+            repeat(numberOfPlayers) {
+                val randomValue = Random.nextDouble(0.0, 1.0)
+                if (randomValue < probability) {
+                    numberOfWinningPlayers++
+                }
+            }
+            numberOfWins[iterator] = numberOfWinningPlayers
+            numberOfWinningPlayers = 0
+        }
+        return numberOfWins
+    }
+
+    // calculateWin function calculate players win based on accumulation, number of others
+    // winners and player's score.
+    private fun calculateWin(cumulate: Double=0.0,
+                             winners: IntArray
+                             , score: Int = 0): Double {
+
+        return when (score) {
+            6 -> (cumulate * 0.44) / (winners[0] + 1)
+            5 -> (cumulate * 0.08) / winners[1] + 1
+            4 -> (cumulate * 0.48) / winners[2] + 1
+            3 -> (cumulate * 0.48) / winners[3] + 1
             else -> 0.0
         }
     }
